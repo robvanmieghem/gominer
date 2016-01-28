36dfc59a1e0310aa8f6258269ac871e3fb961179
Rob Van Mieghem <rob@vanmieghemcloud.com>
2016-01-28 17:00:35 +0100

    siad requires the user agent to be set correctly

--- network.go
+++ network.go
@@ -11,7 +11,16 @@ var getworkurl = "http://localhost:9980/miner/headerforwork"
 var submitblockurl = "http://localhost:9980/miner/submitheader"
 
 func getHeaderForWork() (target, header []byte, err error) {
-	resp, err := http.Get(getworkurl)
+	client := &http.Client{}
+
+	req, err := http.NewRequest("GET", getworkurl, nil)
+	if err != nil {
+		return
+	}
+
+	req.Header.Add("User-Agent", "Sia-Agent")
+	resp, err := client.Do(req)
+
 	if err != nil {
 		return
 	}
@@ -34,6 +43,15 @@ func getHeaderForWork() (target, header []byte, err error) {
 }
 
 func submitHeader(header []byte) (err error) {
-	_, err = http.Post(submitblockurl, "", bytes.NewReader(header))
+	req, err := http.NewRequest("POST", submitblockurl, bytes.NewReader(header))
+	if err != nil {
+		return
+	}
+
+	req.Header.Add("User-Agent", "Sia-Agent")
+
+	client := &http.Client{}
+	_, err = client.Do(req)
+
 	return
 }
