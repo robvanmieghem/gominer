2980edb4c5eab7c7b60d93301b03cc2057379549
acterhd <acterhd@gmail.com>
2016-09-20 06:12:45 +0800

    bats

--- bld.bat
+++ bld.bat
@@ -1,2 +1,2 @@
-call ioc64.exe -input=kernel-simd.cl -cmd=build -spirv64 -simd=avx2 -device=cpu_2_1
+call ioc64.exe -input=kernel-simd.cl -cmd=build -spirv64 -device=cpu_2_1 -simd=avx2
 pause 
--- mine.bat
+++ mine.bat
@@ -1,4 +1,4 @@
-call bcl.bat
+::call bcl.bat
 
 ::gominer -I 20 -cpu -E 0,1,3,4 -H sia-eu1.nanopool.org:9980 -Q "address=c525846e367cb05257fa3ab7dca0e65ed39faa7e64a17519a0398b19292123c00e2b860423e0&worker=win&email=acterhd@gmail.com"
 gominer -I 20 -cpu -E 0,1 -H siamining.com:9980 -Q "address=c525846e367cb05257fa3ab7dca0e65ed39faa7e64a17519a0398b19292123c00e2b860423e0&worker=win"
