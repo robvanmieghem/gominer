f7eb2d9f3ae592e14f0c9599c7e03dc16eb727c4
Rob Van Mieghem <rob@vanmieghemcloud.com>
2016-11-13 10:29:01 +0100

    Connect to the stratum server and subscribe to mining notifications
    set_difficulty is already implemented, the mining.notify still needs to be implemented

--- clients/siastratum.go
+++ clients/siastratum.go
@@ -2,12 +2,19 @@ package clients
 
 import (
 	"errors"
+	"log"
 	"math/big"
+	"sync"
+
+	"github.com/robvanmieghem/gominer/clients/stratum"
 )
 
 //SiaStratumClient is a sia client using the stratum protocol
 type SiaStratumClient struct {
-	connectionstring string
+	connectionstring   string
+	stratumclientMutex sync.Mutex // protects following
+	stratumclient      *stratum.Client
+	target             Target
 }
 
 const (
@@ -22,7 +29,40 @@ type (
 
 //Start connects to the stratumserver and processes the notifications
 func (sc *SiaStratumClient) Start() {
-
+	sc.stratumclientMutex.Lock()
+	defer func() {
+		sc.stratumclientMutex.Unlock()
+	}()
+	sc.stratumclient = &stratum.Client{}
+	//In case of an error, drop the current stratumclient and restart
+	sc.stratumclient.ErrorCallback = func(err error) {
+		log.Println("Error in connection to stratumserver:", err)
+		sc.stratumclient.Close()
+		sc.Start()
+	}
+	//Subscribe to difficulty changes
+	sc.stratumclient.SetNotificationHandler("mining.set_difficulty", func(params []interface{}) {
+		if params == nil || len(params) < 1 {
+			log.Println("No difficulty parameter supplied by stratum server")
+			return
+		}
+		diff, ok := params[0].(float64)
+		if !ok {
+			log.Println("Invalid difficulty supplied by stratum server:", params[0])
+			return
+		}
+		log.Println("Stratum server changed difficulty to", diff)
+		sc.setDifficulty(diff)
+	})
+	//Connect to the stratum server
+	log.Println("Connecting to", sc.connectionstring)
+	sc.stratumclient.Dial(sc.connectionstring)
+	//TODO: proper response handling
+	err := sc.stratumclient.Call("mining.subscribe", []string{}, nil)
+	if err != nil {
+		//Closing the connection will cause the client to generate an error, resulting in te errorhandler to be triggered
+		sc.stratumclient.Close()
+	}
 }
 
 // IntToTarget converts a big.Int to a Target.
@@ -58,8 +98,17 @@ func difficultyToTarget(difficulty float64) (target Target, err error) {
 	return
 }
 
+func (sc *SiaStratumClient) setDifficulty(difficulty float64) {
+	target, err := difficultyToTarget(difficulty)
+	if err != nil {
+		log.Println("Error setting difficulty to ", difficulty)
+	}
+	sc.target = target
+}
+
 //GetHeaderForWork fetches new work from the SIA daemon
 func (sc *SiaStratumClient) GetHeaderForWork() (target, header []byte, err error) {
+	err = errors.New("Not implemented yet")
 	return
 }
 
--- clients/stratum/stratum.go
+++ clients/stratum/stratum.go
@@ -2,24 +2,150 @@
 // This is normal jsonrpc but the go standard library is insufficient since we need features like notifications.
 package stratum
 
-import "encoding/json"
+import (
+	"bufio"
+	"encoding/json"
+	"net"
+	"sync"
+)
 
-// Request : A remote method is invoked by sending a request to the remote stratum service.
-type Request struct {
+// request : A remote method is invoked by sending a request to the remote stratum service.
+type request struct {
 	Method string   `json:"method"`
 	Params []string `json:"params"`
 	ID     uint64   `json:"id"`
 }
 
-// Response is the stratum server's response on a Request
-type Response struct {
-	ID     uint64           `json:"id"`
-	Result *json.RawMessage `json:"result"`
-	Error  error            `json:"error,string"`
+// response is the stratum server's response on a Request
+type response struct {
+	ID           uint64           `json:"id"`
+	Result       *json.RawMessage `json:"result"`
+	Error        error            `json:"error,string"`
+	notification `json:",inline"`
 }
 
-// Notification is a special kind of Request, it has no ID and is sent from the server to the client
-type Notification struct {
-	Method string           `json:"method"`
-	Params *json.RawMessage `json:"params"`
+// notification is a special kind of Request, it has no ID and is sent from the server to the client
+type notification struct {
+	Method string        `json:"method"`
+	Params []interface{} `json:"params"`
+}
+
+//ErrorCallback is the type of function that be registered to be notified of errors requiring a client
+// to be dropped and a new one to be created
+type ErrorCallback func(err error)
+
+//NotificationHandler is the signature for a function that handles notifications
+type NotificationHandler func(args []interface{})
+
+// Client maintains a connection to the stratum server and (de)serializes requests/reponses/notifications
+type Client struct {
+	socket               net.Conn
+	seqmutex             sync.Mutex // protects following
+	seq                  uint64
+	ErrorCallback        ErrorCallback
+	notificationHandlers map[string]NotificationHandler
+}
+
+//Dial connects to a stratum+tcp at the specified network address.
+// This function is not threadsafe
+// If an error occurs, it is both returned here through the ErrorCallback of the Client
+func (c *Client) Dial(host string) (err error) {
+	c.socket, err = net.Dial("tcp", host)
+	if err != nil {
+		c.dispatchError(err)
+		return
+	}
+	go c.Listen()
+	return
+}
+
+//Close releases the tcp connection
+func (c *Client) Close() {
+	if c.socket != nil {
+		c.socket.Close()
+	}
+}
+
+//SetNotificationHandler registers a function to handle notification for a specific method.
+// This function is not threadsafe and all notificationhandlers should be set prior to calling the Dial function
+func (c *Client) SetNotificationHandler(method string, handler NotificationHandler) {
+	if c.notificationHandlers == nil {
+		c.notificationHandlers = make(map[string]NotificationHandler)
+	}
+	c.notificationHandlers[method] = handler
+}
+
+func (c *Client) dispatchNotification(n notification) {
+	if c.notificationHandlers == nil {
+		return
+	}
+	if notificationHandler, exists := c.notificationHandlers[n.Method]; exists {
+		notificationHandler(n.Params)
+	}
+}
+
+func (c *Client) dispatch(r response) {
+	if r.ID == 0 {
+		c.dispatchNotification(r.notification)
+		return
+	}
+	//TODO: dispatch normal response
+}
+
+func (c *Client) dispatchError(err error) {
+	if c.ErrorCallback != nil {
+		c.ErrorCallback(err)
+	}
+}
+
+//Listen reads data from the open connection, deserializes it and dispatches the reponses and notifications
+// This is a blocking function and will continue to listen until an error occurs (io or deserialization)
+func (c *Client) Listen() {
+	for {
+		rawmessage, err := bufio.NewReader(c.socket).ReadString('\n')
+		if err != nil {
+			c.dispatchError(err)
+			return
+		}
+		r := response{}
+		err = json.Unmarshal([]byte(rawmessage), &r)
+		if err != nil {
+			c.dispatchError(err)
+			return
+		}
+		c.dispatch(r)
+	}
+}
+
+//Call invokes the named function, waits for it to complete, and returns its error status.
+func (c *Client) Call(serviceMethod string, args []string, reply interface{}) (err error) {
+	r := request{Method: serviceMethod, Params: args}
+
+	c.seqmutex.Lock()
+	c.seq++
+	r.ID = c.seq
+	c.seqmutex.Unlock()
+
+	rawmsg, err := json.Marshal(r)
+	if err != nil {
+		return
+	}
+	//TODO: call, err := c.registerRequest(r)
+
+	_, err = c.socket.Write(rawmsg)
+	if err != nil {
+		//TODO: c.cancelRequest(r.ID)
+		return
+	}
+	_, err = c.socket.Write([]byte("\n"))
+	if err != nil {
+		//TODO: c.cancelRequest(r.ID)
+		return
+	}
+	//TODO:
+	// call.SetTimeout(10, func() {
+	// 	c.cancelRequest(r.ID)
+	// })
+	// return call.Wait()
+	return
 }
